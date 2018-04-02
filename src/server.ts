8c9ed7521726bd81e6eddd23325442e6ec959e2f
Moritz Tomasi <moritz@zoomsquare.com>
2018-04-02 13:39:30 +0200

    added server endpoints.

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,20 @@ async function run() {
     // initiate the seeder and seed the channels
     let seeder = new Seeder();
     await seeder.seed(channels);
+
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+    
+    app.get("/api/channels", (req, res) => {
+        res.status(200).json(seeder.getSeedingChannels());
+    });
+    
+    app.listen(2000, () => {
+        console.log(`server started and running on port 2000`);
+    });
 }
 
 run();
